5c884f25593f8839aa767a30fb5af231e6577a52
Juan Jose Millan <juanjose@wenuwork.cl>
2019-04-29 15:16:42 -0400

    se agrega codigo de gateway

---
+++ pycom-fragmentation/main.py
@@ -0,0 +1,45 @@
+from network import LoRa
+import socket
+import time
+import ubinascii
+
+# Initialise LoRa in LORAWAN mode.
+# Please pick the region that matches where you are using the device:
+# Asia = LoRa.AS923
+# Australia = LoRa.AU915
+# Europe = LoRa.EU868
+# United States = LoRa.US915
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.AU915)
+
+# create an OTAA authentication parameters
+app_eui = ubinascii.unhexlify('C131647763443EC7')
+app_key = ubinascii.unhexlify('F7398384004F78C0787F39A3C03853F1')
+
+# join a network using OTAA (Over the Air Activation)
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=5)
+
+# wait until the module has joined the network
+while not lora.has_joined():
+    time.sleep(2.5)
+    print('Not yet joined...')
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 4)
+
+# make the socket blocking
+# (waits for the data to be sent and for the 2 receive windows to expire)
+s.setblocking(True)
+
+# send some data
+s.send(bytes([0x01, 0x02, 0x03]))
+
+# make the socket non-blocking
+# (because if there's no data received it will block forever...)
+s.setblocking(False)
+
+# get any data received (if any...)
+data = s.recv(64)
+print(data)
---
+++ pycom-lorawan-gateway/config.py
@@ -0,0 +1,26 @@
+""" LoPy LoRaWAN Nano Gateway configuration options """
+
+import machine
+import ubinascii
+
+WIFI_MAC = ubinascii.hexlify(machine.unique_id()).upper()
+# Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
+GATEWAY_ID = WIFI_MAC[:6] + "FFFE" + WIFI_MAC[6:12]
+
+#SERVER = 'router.brazil.thethings.network'
+SERVER = 'router.eu.thethings.network'
+#SERVER = 'router.us-west.thethings.network'
+#SERVER = 'router.us.thethings.network'
+PORT = 1700
+
+NTP = "pool.ntp.org"
+NTP_PERIOD_S = 3600
+
+WIFI_SSID = 'Alumnos-DIE'
+WIFI_PASS = '17alumnosdie17'
+
+# for US915
+#LORA_FREQUENCY = 915020000 
+LORA_FREQUENCY = 916800000
+LORA_GW_DR = "SF10BW125" # DR_0
+LORA_NODE_DR = 3
